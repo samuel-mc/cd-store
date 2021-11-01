f5fbcbf1ec5928a0262fa4bd2854302e25fbfde2
Samuel Medina <smdoomed@gmail.com>
2021-11-01 15:14:06 -0600

    [add] Add Album form

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import AppContext from '../context/AppContex'
+import AppContext from '../context/AppContex';
+import '../style/AddAlbum.css';
 
 const AddAlbum = () => {
+
+    const [ addArtist, setAddArtist ] = React.useState(false);
+    const [ addGenre, setAddGenre ] = React.useState(false);
 
     const { artists } = React.useContext(AppContext);
     const { genres } = React.useContext(AppContext);
@@ -10,51 +14,85 @@ const AddAlbum = () => {
         console.log(genres);
     })
 
+    const handleAddArtist = () => {
+        setAddArtist(!addArtist);
+    }
+    const handleAddGenre = () => {
+        setAddGenre(!addGenre);
+    }
+
     return (
         <div className="artist__form">
             <form action="">
-                <h1>Agregar Album</h1>
-                <div className="artist__form__input">
+                <div className="form__title">
+                    <h1>Agregar Album</h1>
+                </div>
+                <div className="form__item">
                     <label htmlFor="name">Titulo</label>
-                    <input type="text" id="name" placeholder="Titulo"/>
+                    <input type="text" id="name" placeholder="El mejor disco del mundo"/>
                 </div>
-                <div className="artist__form__input">
+                <div className="form__item">
                     <label htmlFor="year">Año</label>
-                    <input type="number" id="year" min="1950" max="2050" placeholder="AAAA"/>
+                    <input type="number"  id="year" min="1950" max="2050" placeholder="AAAA"/>
                 </div>
-                <div className="artist__form__input">
+                <div className="form__item">
                     <label htmlFor="cover">URL de la Portada</label>
                     <input type="text" id="cover" placeholder="http://www.image.com"/>
                 </div>
-                <div className="artist__form__input">
+                <div className="form__item">
                     <label htmlFor="description">Descripción</label>
                     <input type="text" id="desctiption" placeholder="Acá va una descripción ..."/>
                 </div>
-                <div className="artist__form__input">
+                <div className="form__item">
                     <label htmlFor="price">Precio</label>
-                    <input type="number" id="price" step="0.1" placeholder="199.9"/>
+                    <input type="number" id="price" min="0" step="0.1" placeholder="199.9"/>
                 </div>
-                <div className="artist__form__input">
+                <div className="form__item">
                     <label htmlFor="stock">Stock</label>
-                    <input type="number" id="stock" step="1" placeholder="50"/>
+                    <input type="number" id="stock" min="0" step="1" placeholder="50"/>
                 </div>
-                <div className="artist__form__input">
+                <div className="form__item">
                     <label htmlFor="stock">Artista</label>
-                    <select name="id_artist" id="id_artist">
-                        <option value="">Seleccione un Artista</option>
-                        {
-                            artists.length !== 0 && artists.artists.map(artist => <option value={artist.id_artist} key={artist.id_artist}>{artist.name}</option>)
+                    <div className="form__select">
+                        {addArtist
+                        ?   <>
+                                <input type="text" id="name" placeholder="Gerardo Camino" className=""/>
+                                <button type="button" onClick={() => handleAddArtist()} className="button--cancel">Cancelar</button>
+                            </>
+                        :   <>
+                                <select name="id_artist" id="id_artist" hidden={addArtist}>
+                                    <option value="">Seleccione un Artista</option>
+                                    {
+                                        artists.length !== 0 && artists.artists.map(artist => <option value={artist.id_artist} key={artist.id_artist}>{artist.name}</option>)
+                                    }
+                                </select>
+                                <button type="button" onClick={() => handleAddArtist()}>Nuevo</button>
+                            </>
                         }
-                    </select>
+                    </div>
                 </div>
-                <div className="artist__form__input">
+                <div className="form__item">
                     <label htmlFor="stock">Genero</label>
-                    <select name="id_genre" id="id_genre">
-                        <option value="">Seleccione un Género</option>
-                        {
-                            genres.length !== 0 && genres.map(genre => <option value={genre.id_genre} key={genre.id_genre}>{genre.name}</option>)
+                    <div className="form__select">
+                        {addGenre
+                        ?   <>
+                                <select name="id_genre" id="id_genre">
+                                    <option value="">Seleccione un Género</option>
+                                    {
+                                        genres.length !== 0 && genres.map(genre => <option value={genre.id_genre} key={genre.id_genre}>{genre.name}</option>)
+                                    }
+                                </select>
+                                <button type="button" onClick={() => handleAddGenre()} className="button--new">Nuevo</button>
+                            </>
+                        :   <>
+                                <input type="text" id="name" placeholder="Post Pop" />
+                                <button type="button" onClick={() => handleAddGenre()} className="button--cancel">Cancelar</button>
+                            </>
                         }
-                    </select>
+                    </div>
+                </div>
+                <div className="form__button">
+                    <button type="button" className="button--save">Guardar</button>
                 </div>
             </form>
         </div>
